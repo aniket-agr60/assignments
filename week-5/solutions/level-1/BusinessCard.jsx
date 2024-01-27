5f28d7d627f6b744b80b8350dc901807b25e59ec
Aniket <aniketagrahari60@gmail.com>
2024-01-27 20:53:20 +0530

    testing

--- week-5/solutions/level-1/BusinessCard.jsx
+++ week-5/solutions/level-1/BusinessCard.jsx
@@ -4,13 +4,13 @@ export function BusinessCard(props) {
         <h2 style={styles.name}>{props.name}</h2>
         <p style={styles.description}>{props.description}</p>
         <h3 style={styles.interestsHeader}>Interests</h3>
-        <ul style={styles.interestsList}>
+        {/* <ul style={styles.interestsList}>
           {props.interests.map((interest) => (
             <li key={interest} style={styles.interestItem}>
               {interest}
             </li>
           ))}
-        </ul>
+        </ul> */}
         <div style={styles.socialLinks}>
           <a href={props.linkedin} target="_blank" rel="noopener noreferrer" style={{...styles.link, marginLeft: '0px'}}>
             LinkedIn
